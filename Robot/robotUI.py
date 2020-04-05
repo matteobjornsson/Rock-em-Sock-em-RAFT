278fa7eb05bf82e6b5bedb2da6e29a4354697797
amylinck <amy.peerlinck@student.montana.edu>
2020-04-05 16:04:17 -0600

    Simple UI finished

--- Robot/robotUI.py
+++ Robot/robotUI.py
@@ -1,6 +1,42 @@
-from Robot.robot_logic import Robot
+from Robot.robot_logic import *
+
 
 class UI:
+    """
 
+    """
     def __init__(self):
-        self.Robot = Robot(color='blue')
+        self.robot = Robot(color='blue')
+        self.running_game = True
+
+    def run_game(self):
+        print("Welcome to ROCK 'EM SOCK 'EM!\nYou can use the following commands to move your "
+              "robot\n---------------------------------------")
+        print("Q = punch with left \nW = punch with right \nA = block with left \nS = block with right \n0 = Exit")
+        while self.running_game:
+            user_choice = input()
+            if user_choice == 'Q':
+                try:
+                    self.robot.punch_with_left()
+                except RobotBlockedError:
+                    print("robot cannot punch right now")
+            elif user_choice == 'W':
+                try:
+                    self.robot.punch_with_right()
+                except RobotBlockedError:
+                    print("robot cannot punch right now")
+            elif user_choice == 'A':
+                self.robot.block_with_left()
+            elif user_choice == 'S':
+                self.robot.block_with_right()
+            elif user_choice == '0':
+                self.running_game = False
+                print("We hope you had fun! Goodbye")
+            else:
+                print("not a valid input")
+
+
+if __name__ == '__main__':
+
+    ui = UI()
+    ui.run_game()
